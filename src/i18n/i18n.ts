c0e6d0936e98d78a06382b9187e363f612aa8c83
Hongsophaline <hong.sophaline@institute.pse.ngo>
2026-03-27 10:46:25 +0700

    add translate

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,18 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend";
 
-i18n
-  .use(Backend) // This fetches the JSON files from your public folder via HTTP
-  .use(initReactI18next)
-  .init({
-    lng: "en",
-    fallbackLng: "en",
-    backend: {
-      // Path to your public folder files
-      loadPath: "/locales/{{lng}}.json",
-    },
-    interpolation: { escapeValue: false },
-  });
+// Import JSON translation files
+import en from "./locales/en.json";
+import km from "./locales/km.json";
+import zh from "./locales/zh.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    km: { translation: km },
+    zh: { translation: zh },
+  },
+  lng: "en", // default language
+  fallbackLng: "en", // fallback if key missing
+  interpolation: { escapeValue: false },
+});
 
 export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./i18n/i18n"; // <-- import i18n
-
+import "./i18n/i18n"; //
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
